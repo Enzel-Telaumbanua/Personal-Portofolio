ed38e37f5dab42b692bb555030b3d1b6bfad6dd5
Enzel-Telaumbanua <enzeltelaumbanua04@gmail.com>
2024-06-08 13:13:50 +0700

    mengatur ukuran nama saja

--- src/components/contents/ExperienceComponent.vue
+++ src/components/contents/ExperienceComponent.vue
@@ -2,7 +2,7 @@
     <div class="slide-right w-full rounded-md bg-background sm:p-5 max-sm:p-3 shadow-custom">
         <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">Experience</span></h1>
         <ul class="flex flex-col space-y-1 list-disc pl-5 sm:pr-3 max-sm:text-sm">
-            <li>Ketua Organinasi</li>
+            <li>Ketua Humas</li>
             <p class="text-sm">Pernah menjabat selama 2 tahun (4 semester) sebagai ketua di organisasi Budidarma Programming Club dari tahun 2022 sampai tahun 2024.</p>
             <li>Instruktur Frontend</li>
             <p class="text-sm">Menjadi mentor atau instruktur pada organisasi Budidarma Programming Club pada tahun 2023 selama 1 tahun (2 semester).</p>
